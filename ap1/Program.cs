4a512f7d33f8558cf9387f8fe27864a290db4acb
SekitoFanAcc <120167023+SekitoFanAcc@users.noreply.github.com>
2022-12-09 08:51:15 +0100

    Add files via upload

---
+++ ap1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Introduce una palabra");
+
+string entrada = Console.ReadLine();
+
+char[] palabra = entrada.ToCharArray();
+
+for (int i = 0; i < palabra.Length; i++)
+{
+
+    System.Console.WriteLine(palabra[i]);
+}
